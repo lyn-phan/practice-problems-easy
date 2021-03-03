d03ddacf80aff4976766f6502f48882b73f2781a
Lynda Phan <lynda.kim.phan@gmail.com>
2021-03-03 00:23:11 +0000

    Completed prob #2, adding #3

--- practice.py
+++ practice.py
@@ -23,33 +23,23 @@
 
 
 def countTinyPairs(a, b, k):
-    # initialize tiny_list_count = 0
     tiny_list_count = 0
-    list_of_concat = []
+    concat_list = []
 
-    # iterate through a normally
-    for i in a:
-        for j in b[::-1]:
-            # result = str(a[i] + b[j])
-            # print(result)
-            # print(type(result))
-            str_i = str(i)
-            str_j = str(j)
-            concat = str_i + str_j
-            convert_to_int = int(concat)
-            list_of_concat.append(convert_to_int)
-            print(list_of_concat)
+    for i, j in zip(a, b):
+        str_i = str(i)
+        str_j = str(j)
+        concat = str_i + str_j
+        convert_to_int = int(concat)
+        concat_list.append(convert_to_int)
 
-    # for x in list_of_concat:
-    #     if x < k:
-    #         tiny_list_count += 1
-    # return tiny_list_count
+    for x in concat_list:
+        if x < k:
+            tiny_list_count += 1
+    return tiny_list_count
 
 print(countTinyPairs(a = [16, 1, 4, 2, 14], b = [7, 11, 2, 0, 15], k = 743))
-    
-    # iterate through b by -1
-    #concatenate each pair
-    #check if pair is < k
-    # if less than k, increment count
-    # return list count
+  
+
+
 
